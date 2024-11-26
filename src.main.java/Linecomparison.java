a7189451fd3e42540b529e2bac1e2e0296197762
yogeshrkatole@gmail.com <yogeshrkatole@gmail.com>
2024-11-26 14:48:13 +0530

    Compare Lines

--- src.main.java/Linecomparison.java
+++ src.main.java/Linecomparison.java
@@ -18,21 +18,36 @@ public class LineComparison {
        LineComparison otherLine = (LineComparison) obj;
        return Double.compare(this.calculateLength(), otherLine.calculateLength()) == 0;
    }
+   public int compareTo(LineComparison otherLine) {
+	   return Double.compare(this.calculateLength(), otherLine.calculateLength());
+   }
   public static void main(String[] args) {
-	System.out.println("Welcome to Line Comparison Computation Program");
+	System.out.println("Welcome to Line Comparison Computation Program\n");
 	double x1=3.0,y1=2.0,x2=7.0,y2=1.0;
 	double x11=3.0,y11=2.0,x21=7.0,y21=11.0;
-	LineComparison lineComparison1=new LineComparison(x11,y11,x21,y21);
-	LineComparison lineComparison2=new LineComparison(x1,y1,x2,y2);
+	double x12=3.0,y12=2.0,x22=7.0,y22=11.0;
+	LineComparison lineComparison1=new LineComparison(x12,y12,x22,y22);
+	LineComparison lineComparison2=new LineComparison(x11,y11,x21,y21);
+	LineComparison lineComparison3=new LineComparison(x1,y1,x2,y2);
 	  double lengthOfLine1=lineComparison1.calculateLength();
 	System.out.println("The Length of First Line: "+lengthOfLine1);
-	 double lengthOfLine2=lineComparison1.calculateLength();
+	 double lengthOfLine2=lineComparison2.calculateLength();
      System.out.println("The Length of Second Line: "+lengthOfLine2);
-     if(lineComparison1.equals(lineComparison2)) {
-    	 System.out.println("Lines are equal");
-     }else {
-    	 System.out.println("Lines are not equal");
-
-     }
+     double lengthOfLine3=lineComparison3.calculateLength();
+     System.out.println("The Length of Second Line: "+lengthOfLine3);
+     
+     System.out.println("\nComparison Results:");
+     System.out.println("Line 1 vs Line 2: "+ compareResult(lineComparison1.compareTo(lineComparison2)));
+     System.out.println("Line 2 vs Line 3: "+ compareResult(lineComparison2.compareTo(lineComparison3)));
+     System.out.println("Line 3 vs Line 1: "+ compareResult(lineComparison3.compareTo(lineComparison1)));
+  }
+  private static String compareResult(int result) {
+      if (result == 0) {
+          return "Lines are equal.";
+      } else if (result > 0) {
+          return "First line is longer.";
+      } else {
+          return "First line is shorter.";
+      }
   }
 }
